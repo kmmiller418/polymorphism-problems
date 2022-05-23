0f47b91f64ac91847fda6f80edd4c29cdff8b5a1
kmiller <kmmiller418@gmail.com>
2022-05-23 18:33:27 -0400

    Add Animals project

---
+++ Animals/src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        Dog dog = new Dog();
+        dog.bark();
+        dog.eat();
+
+        Dog fido = new Dog("Fido");
+        fido.bark();
+
+        Cat cat = new Cat();
+        cat.purr();
+        cat.eat();
+
+        Cat garfield = new Cat("Garfield");
+        garfield.purr();
+
+        fido.makeNoise();
+        garfield.makeNoise();
+
+    }
+}
